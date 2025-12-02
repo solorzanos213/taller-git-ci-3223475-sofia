39dfa9b0ba22d6a7719aaab9e7318d84acf6405f
Sofia Solorzano <solorzanos213@gmail.com>
2025-12-02 16:46:00 -0500

    fix: format code with prettier

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 function agregarItem(texto) {
-  const lista = document.getElementById("lista");
-  const li = document.createElement("li");
+  const lista = document.getElementById('lista');
+  const li = document.createElement('li');
   li.textContent = texto;
   lista.appendChild(li);
 }
